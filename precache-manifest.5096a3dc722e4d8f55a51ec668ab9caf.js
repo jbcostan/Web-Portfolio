fdec02d124f46b14aba11aa913e6cda7016bfab3
jbcostan <jbcostan@hotmail.com>
2020-05-15 01:48:00 -0400

    Updates

---
+++ precache-manifest.5096a3dc722e4d8f55a51ec668ab9caf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e539b4db4ae8f9150437bf1b09f0ea9",
+    "url": "/Web=Portfolio/index.html"
+  },
+  {
+    "revision": "bd71a6ffaf5447338574",
+    "url": "/Web=Portfolio/static/css/main.3226e4cb.chunk.css"
+  },
+  {
+    "revision": "33317fa825b74dae023c",
+    "url": "/Web=Portfolio/static/js/2.d74f3884.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Web=Portfolio/static/js/2.d74f3884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd71a6ffaf5447338574",
+    "url": "/Web=Portfolio/static/js/main.5944e107.chunk.js"
+  },
+  {
+    "revision": "8e7f6858f6bfaca03815",
+    "url": "/Web=Portfolio/static/js/runtime-main.8af27789.js"
+  },
+  {
+    "revision": "89459957978bf46cdd8696999ce7790a",
+    "url": "/Web=Portfolio/static/media/bookstore1.89459957.jpg"
+  },
+  {
+    "revision": "6b7be7dd3d51f1c26b390f7250edebc4",
+    "url": "/Web=Portfolio/static/media/bookstore2.6b7be7dd.png"
+  },
+  {
+    "revision": "59fbae9427795566ff89744f50f9e490",
+    "url": "/Web=Portfolio/static/media/github_icon.59fbae94.png"
+  },
+  {
+    "revision": "c0f94d4da3436208e5a90a65feb3f867",
+    "url": "/Web=Portfolio/static/media/github_page.c0f94d4d.png"
+  },
+  {
+    "revision": "a6d5ed8acba964e500442fc425d9e2ef",
+    "url": "/Web=Portfolio/static/media/profile.a6d5ed8a.jpg"
+  },
+  {
+    "revision": "c1012d2bada0e46608336e40ba33fc54",
+    "url": "/Web=Portfolio/static/media/profile2.c1012d2b.png"
+  },
+  {
+    "revision": "268d37f21ad4823b1207d63b4e995f20",
+    "url": "/Web=Portfolio/static/media/uwin_logo.268d37f2.jpg"
+  }
+]);
